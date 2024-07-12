1fa8eea44ecfaa2d36f80d9219b4c3e977932217
Hank Shen <swh@admpub.com>
2024-07-12 20:39:54 +0800

    update

--- imgparse/imgparse.go
+++ imgparse/imgparse.go
@@ -1,23 +1,25 @@
 package imgparse
 
 import (
-    "errors"
-    "io"
+	"errors"
+	"io"
 )
 
 func ParseRes(r io.Reader, content string) (int, int, error) {
-    switch content {
-    case "gif":
-        return parseGIF(r)
-    case "jpeg":
-        return parseJPEG(r)
-    case "png":
-        return parsePNG(r)
-    case "webp":
-        return parseWebP(r)
-    case "webpll":
-        return parseWebP(r)
-    default:
-        return 0, 0, errors.New("Unknown content type.")
-    }
+	switch content {
+	case "gif":
+		return parseGIF(r)
+	case "jpeg":
+		return parseJPEG(r)
+	case "png":
+		return parsePNG(r)
+	case "webp":
+		return parseWebP(r)
+	case "webpll":
+		return parseWebP(r)
+	case "svg":
+		return 0, 0, nil
+	default:
+		return 0, 0, errors.New("Unknown content type.")
+	}
 }
